2730e86bced67548a0a62b9e0ea440056ccf4fc4
Achille KINGNON <yongkalifachille@gmail.com>
2025-12-03 18:17:20 +0000

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import { BrowserRouter } from "react-router-dom";
 import App from "./App";
-import "./styles.css"; // chemin relatif
+import "./style.css"; // chemin relatif
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
